17267fe2b49108f13c5c00909d7df56878e791d8
Carlos Joaquim <carlos.joaquim@dextra-sw.com>
2019-08-22 09:55:03 -0300

    Incluindo autenticação e alteração de usuários

--- server/src/app/controllers/UserController.js
+++ server/src/app/controllers/UserController.js
@@ -31,6 +31,49 @@ class UserController {
       email,
     });
   }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { email, oldPassword } = req.body;
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'Email is already in use.' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(400).json({ error: 'OldPassword does not match.' });
+    }
+    const { id, name, provider } = await user.update(req.body);
+    return res.json({
+      id,
+      name,
+      provider,
+    });
+  }
 }
 
 export default new UserController();
---
+++ server/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '955b95410d45daae316ffd7a85c37fc6',
+  expiresIn: '7d',
+};
